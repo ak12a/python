853e0444acdb9f088dff47fcdb5d6771b8782c27
ak12a <arvindrhce12@gmail.com>
2022-05-15 19:18:56 +0530

    Python-demo

---
+++ demo-python/l2.py
@@ -0,0 +1,14 @@
+# Print Hello World 
+print("Hello World")
+
+#Print Hello world 100Times 
+
+print("Ha"*100)
+
+
+
+
+## Print Today date ### 
+from datetime import date
+today = date.today()
+print("Today's date:" , today )
